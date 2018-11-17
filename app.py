6fd5d2f8249a137240d7a21651bca7b946b6aaae
anthopark <anthopark2015@gmail.com>
2018-11-17 15:02:59 -0800

    some work on reddit API

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #reddit
-    subreddit_name = 'UCI' 
+    subreddit_name = 'UCI'
     reddit_instance = reddit.create_reddit_instance(read_only=True)
-    reddit_posts = reddit.ten_top_posts(reddit_instance,subreddit_name)
+    reddit_posts = []
+    for p in reddit.ten_top_posts(reddit_instance,subreddit_name):
+        reddit_posts.append(p)
+
 
     #twitter
 
@@ -18,7 +21,9 @@ def index():
 
 
     
-    return render_template('index.html',redditPosts = top_posts,name = subreddit_name)
+    return render_template('index.html',redditPosts = reddit_posts, name = subreddit_name)
 
 if __name__ == "__main__":
+    
     app.run(debug = True) #Set debug = False in a production environment
+    
