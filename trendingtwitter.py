957835c33dd763918f3d95d129769047d4037d5a
jessicashu7 <jessicashu7@gmail.com>
2018-11-17 18:23:26 -0800

    twitter api

--- trendingtwitter.py
+++ trendingtwitter.py
@@ -8,7 +8,7 @@ def create_twitter_instance():
 
     with open('credentialstwitter.json') as creds:
         credentials = json.load(creds)
-        api = twitter.api(consumer_key=credentials['consumer_key'],
+        api = twitter.Api(consumer_key=credentials['consumer_key'],
                       consumer_secret=credentials['consumer_secret'],
                       access_token_key=credentials['access_token_key'],
                       access_token_secret=credentials['access_token_secret'])
@@ -16,15 +16,10 @@ def create_twitter_instance():
 
 
 def five_trending(api):
-    return api.GetTrendsCurrent()
+    return api.GetTrendsCurrent()[:4]
 
 if __name__ == '__main__':
     '''Experiment with your reddit function here. Examine the output on the console.'''
 
     t_api = create_twitter_instance()
     print(five_trending(t_api))
-    #posts = ten_top_posts(reddit_instance, subreddit_name)
-
-    #for post in posts:
-    #    print(post.title)
-    #    print(post.author)
